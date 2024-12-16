31dba7fbfd699883c33335b1d40a21bf19023f8e
Maximilien BARRAULT <Maximilien@nextherm.local>
2024-12-16 10:59:29 +0100

    Modification devis in

--- app/protected/devis/page.tsx
+++ app/protected/devis/page.tsx
@@ -1,7 +1,19 @@
 'use client';
 
+import { Suspense } from 'react';
 import DevisBuilder from '@/app/protected/devis/DevisBuilder';
+import { Loader2 } from 'lucide-react';
 
 export default function Page() {
-  return <DevisBuilder />;
+  return (
+    <Suspense 
+      fallback={
+        <div className="flex justify-center items-center min-h-screen">
+          <Loader2 className="animate-spin h-8 w-8 text-[#86BC29]" />
+        </div>
+      }
+    >
+      <DevisBuilder />
+    </Suspense>
+  );
 }
