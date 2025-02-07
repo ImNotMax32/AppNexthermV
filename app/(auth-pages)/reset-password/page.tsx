b4d07390bd68c48518862d9f6cf1471afa3a6066
lveteau <loic.veteau@outlook.fr>
2025-02-07 08:54:08 +0100

    test en production avec le localstorage

--- app/(auth-pages)/reset-password/page.tsx
+++ app/(auth-pages)/reset-password/page.tsx
@@ -35,7 +35,8 @@ export default function ResetPasswordPage() {
             detectSessionInUrl: true,
             autoRefreshToken: false,
             persistSession: true,
-            storage: memoryLocalStorageAdapter(store)
+            // storage: memoryLocalStorageAdapter(store),
+            storage: localStorage,
           }
         }
       );
