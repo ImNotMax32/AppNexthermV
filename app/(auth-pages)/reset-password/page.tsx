14123b9c43ccfcf262dbc0f54eca0085fe673012
lveteau <loic.veteau@outlook.fr>
2025-02-07 08:56:43 +0100

    test sans la redirection

--- app/(auth-pages)/reset-password/page.tsx
+++ app/(auth-pages)/reset-password/page.tsx
@@ -41,7 +41,8 @@ export default function ResetPasswordPage() {
         }
       );
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/auth/callback?redirect=/sign-in&type=recovery`,
+        redirectTo: `${window.location.origin}/auth/callback`,
+        // ?redirect=/sign-in&type=recovery
       });
 
       if (error) {
